c8c3ad71d0cffe5c9fc11e61a3735d3c7eb4e2c1
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-09-28 05:31:44 +0530

    Access token, social auth, user info

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { activateUser, loginUser, logoutUser, registrationUser } from '../controllers/userController';
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken } from '../controllers/userController';
 import { isAuthenticated } from '../middelware/auth';
 const userRouter = express.Router();
 
@@ -7,5 +7,8 @@ userRouter.post('/registration', registrationUser);
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/logout',isAuthenticated, logoutUser);
+userRouter.get('/refresh',updateAccessToken);
+userRouter.get('/me',isAuthenticated, getUserInfo);
+userRouter.post('/social-auth',socialAuth);
 
 export default userRouter;
