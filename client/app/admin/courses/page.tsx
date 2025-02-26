7802ff955f43ec89155435d634443cda0aa6837b
Reeturaj Kumar <reeturajvats587@gmail.com>
2025-02-26 13:29:57 +0530

    course page Ui dashboard

---
+++ client/app/admin/courses/page.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+"use client"
+
+import AdminDashboard from '@/app/components/Admin/AdminDashboard'
+import AdminSidebar from '@/app/components/Admin/Sidebar/AdminSidebar'
+import React from 'react'
+import Heading from '@/app/utils/Heading';
+import AdminProtected from '@/app/hooks/adminProtected';
+import AllCourses from '../../components/Admin/Course/AllCourses';
+
+type Props = {}
+const page = (props: Props) => {
+  return (
+    <div>
+    <AdminProtected>
+    <Heading
+      title="LearnNexus - Admin"
+      description="LearnNexus is a platform that provides a wide range of courses and resources for learning and self-improvement."
+      keywords="Programming,MERN,Redux,React,Nextjs,Machine learning,Data science"
+    />
+    <div className='flex h-screen'>
+      <div className='1500px:w-[50%] w-1/5'>
+      <AdminSidebar/>
+      </div>
+      <div className="w-[85%]">
+      <AdminDashboard/>
+      <AllCourses/>
+      </div>
+    </div>
+    </AdminProtected>
+  </div>
+  )
+}
+
+export default page
