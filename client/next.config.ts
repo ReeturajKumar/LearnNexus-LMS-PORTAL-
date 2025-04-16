7fc229483b5d350851f4b90ea113b02150fbcb9e
Reeturaj Kumar <reeturajvats587@gmail.com>
2025-04-16 16:06:27 +0530

    hydration change

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com","randomuser.me","cdn-icons-png.flaticon.com"],
+    domains: ["res.cloudinary.com", "randomuser.me", "cdn-icons-png.flaticon.com"],
+  },
+  experimental: {
+    reactRoot: true,
+    suppressHydrationWarning: true,
   },
 };
 
--- server/build/utils/jwt.js
+++ server/build/utils/jwt.js
@@ -10,7 +10,8 @@ exports.accessTokenOptions = {
     expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
     maxAge: accessTokenExpire * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
+    secure: true
 };
 exports.refreshTokenOptions = {
     expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
@@ -25,9 +26,9 @@ const sendToken = (user, statusCode, res) => {
     redis_1.redis.set(user._id, JSON.stringify(user));
     //parse enviroments variables
     // only set secure to true in production
-    if (process.env.NODE_ENV === 'production') {
-        exports.accessTokenOptions.secure = true;
-    }
+    // if(process.env.NODE_ENV === 'production'){
+    //   accessTokenOptions.secure = true;
+    // }
     res.cookie("access_token", accessToken, exports.accessTokenOptions);
     res.cookie("refresh_token", refreshToken, exports.refreshTokenOptions);
     res.status(statusCode).json({
