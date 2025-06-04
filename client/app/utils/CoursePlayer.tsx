2803d639833c35eeb809d4f7e7692bbb632859e3
Reeturaj Kumar <reeturajvats587@gmail.com>
2025-06-04 17:01:25 +0530

    Course player 3

--- client/app/utils/CoursePlayer.tsx
+++ client/app/utils/CoursePlayer.tsx
@@ -7,37 +7,51 @@ type Props = {
 };
 
 const CoursePlayer: FC<Props> = ({ videoUrl }) => {
+  const [loading, setLoading] = useState(true);
   const [videoData, setVideoData] = useState({
     otp: "",
     playbackInfo: "",
   });
 
   useEffect(() => {
-    axios
-      .post("https://learnnexus-lms-portal.onrender.com/api/v1/getVdoCipherOTP", {
-        videoId: videoUrl,
-      })
-      .then((res) => {
-        setVideoData(res.data);
-      });
-  }, [videoUrl]);
+  setLoading(true);
+  axios
+    .post("https://learnnexus-lms-portal.onrender.com/api/v1/getVdoCipherOTP", {
+      videoId: videoUrl,
+    })
+    .then((res) => {
+      setVideoData(res.data);
+      setLoading(false);
+    })
+    .catch((err) => {
+      console.error("Failed to fetch OTP:", err);
+      setLoading(false);
+    });
+}, [videoUrl]);
+
+
   return (
     <div style={{position: "relative", paddingTop: "56.25%", overflow: "hidden"}}>
-      {videoData.otp && videoData.playbackInfo !== "" && (
-        <iframe
-          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=Sh4pjwlXiTijM6bC`}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            border: "0",
-          }}
-          allowFullScreen={true}
-          allow="encrypted-media"
-        ></iframe>
-      )}
+     {loading ? (
+  <p>Loading video...</p>
+) : (
+  videoData.otp &&
+  videoData.playbackInfo && (
+    <iframe
+      src={`https://player.vdocipher.com/v2/?otp=${videoData.otp}&playbackInfo=${videoData.playbackInfo}&player=Sh4pjwlXiTijM6bC`}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        border: "0",
+      }}
+      allowFullScreen={true}
+      allow="encrypted-media"
+    ></iframe>
+  )
+)}
     </div>
   );
 };
